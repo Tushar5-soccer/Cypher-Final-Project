721e48baf18a23469afc29dd882ae19190544f46
Tushar Chatterjee <73824428+Tushar5-soccer@users.noreply.github.com>
2023-02-14 17:46:43 +0530

    Add files via upload

---
+++ src_project/main/java/com/example/demo/model/product/Product.java
@@ -0,0 +1,27 @@
+package com.example.demo.model.products;
+
+import lombok.Data;
+
+public @Data class Product {
+
+    private Integer productId;
+    private String title;
+    private Double price;
+    private String reviews;
+    private Double rating;
+    private String image;
+    private Double discount;
+    private String category;
+    private String brand;
+
+
+
+
+
+
+
+
+
+
+
+}
